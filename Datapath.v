e8201baa90a48b4950f85b5a96764b830a9cf0fd
cuongloveyou <55919737+cuongloveyou@users.noreply.github.com>
2019-12-25 11:40:57 +0700

    Add files via upload

--- Datapath.v
+++ Datapath.v
@@ -13,21 +13,27 @@ parameter add = 6'h20;
 parameter sub = 6'h22;
 parameter x16bitZero = 16'h0000;
 parameter x2bitZero = 2'b00;
+parameter beqOp = 6'b000100;
 
 reg [63:0] IF_ID_reg;
 reg [147:0] ID_EX_reg;
 reg [84:0] EX_MEM_reg;
 
 reg [31:0] Imemory [0:1023]; //InstructionMemory
-reg [31:0] PCcurrent, PCnext; 
+reg [31:0] PCcurrent, PCnext, readdata; 
+reg [1:0] check;
 
 reg [31:0] readData1, readData2, signExtend, shiftOut; //Decode
 reg [5:0] opcodeReg;
+reg [31:0] PCnextID;
+reg [4:0] rt;
+reg [4:0] rd;
 
 reg [31:0] ALUresult; // alu
 reg [5:0] WriteAddr;
 reg [5:0] ReadAddr;
 reg [31:0] BranchAddr;
+reg [8:0] controlEX;
 
 reg [31:0] memory [0:31]; // datamem
 reg [31:0] memoryReg[0:31];
@@ -60,7 +66,10 @@ begin
 		memoryReg[25] <= 32'h00000000;
 		memoryReg[31] <= 32'h00000000;			
 		
+		memory[1] <= 32'h00000003;
+		
 		PCnext = 0;	
+		check = 2'b00;
 		
 end
 	
@@ -70,25 +79,44 @@ always @ (posedge clk) //pc	   //InstructionMemory
 	    begin
 	      PCnext = EX_MEM_reg[75:44];
 	    end
-	  PCcurrent = PCnext;
-	  PCnext = PCnext + 4;
-    //readdata <= Imemory[pc>>2]; // Insmem
-    IF_ID_reg = {PCnext, Imemory[PCcurrent>>2]};
+	  if (check == 2'b00)
+	    begin
+	      PCcurrent = PCnext;
+	      PCnext = PCnext + 4;
+        readdata = Imemory[PCcurrent>>2]; // Insmem
+        if (readdata[31:26] == beqOp)
+          begin
+            check = 2'b10;
+          end
+      end
+    else check = check - 1;
 	end			
+
+always @(negedge clk)
+begin
+        IF_ID_reg = {PCnext, readdata};
+end
 
 assign opcode = opcodeReg;
 
-always @(negedge clk) // Decode
+always @(posedge clk) // Decode
   begin
     opcodeReg = IF_ID_reg[31:26];
     readData1 = memoryReg[IF_ID_reg[25:21]];
     readData2 = memoryReg[IF_ID_reg[20:16]];
     if (IF_ID_reg[15] == 1) signExtend = {16'b1111111111111111, IF_ID_reg[15:0]};
-    else signExtend = {16'b00000000000000000, IF_ID_reg[15:0]};
+    else signExtend = {16'b0000000000000000, IF_ID_reg[15:0]};
     shiftOut = {signExtend[29:0], x2bitZero};
-    ID_EX_reg = {control, IF_ID_reg[63:32], readData1, readData2, shiftOut, IF_ID_reg[20:16], IF_ID_reg[15:11]};
-                            // pcnext
+    PCnextID = IF_ID_reg[63:32];
+    rt = IF_ID_reg[20:16];
+    rd = IF_ID_reg[15:11];
   end
+
+always @(negedge clk)
+begin
+  ID_EX_reg = {control, PCnextID, readData1, readData2, shiftOut, rt, rd};
+                            // pcnext
+end
 
 always @(posedge clk) // ALU
 begin
@@ -110,18 +138,21 @@ begin
       end
     lw:
         begin
-          ReadAddr = ID_EX_reg[9:5] + ID_EX_reg[17:12];
-          WriteAddr = ID_EX_reg[4:0];
+          ReadAddr = ID_EX_reg[105:74];
+          WriteAddr = ID_EX_reg[9:5] + ID_EX_reg[17:12];
         end
     sw:
         begin
-          ReadAddr = ID_EX_reg[9:5];
+          //ReadAddr = ID_EX_reg[9:5];
+          ALUresult = ID_EX_reg[73:42];
           WriteAddr = ID_EX_reg[4:0] + ID_EX_reg[17:12];
         end
     beq:
         begin
           if (ID_EX_reg[105:74] == ID_EX_reg[73:42])
             begin
+              $display("%d", ID_EX_reg[137:106]);
+              $display("%d", ID_EX_reg[41:10]);
               BranchAddr = ID_EX_reg[137:106] + ID_EX_reg[41:10];
             end
           else 
@@ -130,24 +161,32 @@ begin
             end
         end
     endcase
-    EX_MEM_reg = {control, BranchAddr, ALUresult, ReadAddr, WriteAddr};
+    controlEX = ID_EX_reg[146:138];
 end
 
-always @(negedge clk) // DataMem
+always @(negedge clk)
+begin
+    EX_MEM_reg = {controlEX, BranchAddr, ALUresult, ReadAddr, WriteAddr};
+end
+
+always @(posedge clk) // DataMem
   begin
     if(EX_MEM_reg[80] == 1) // MemWrite
     begin
       memory[EX_MEM_reg[5:0]] = EX_MEM_reg[43:12]; // writeAddr __ ALUresult
+      $display("sw %b",EX_MEM_reg[43:12]);
     end
     
-    if (EX_MEM_reg[82] == 1) // MemRead MemToReg
+    if (EX_MEM_reg[82] == 1) // MemRead MemToReg lw
     begin
       memoryReg[EX_MEM_reg[5:0]] = memory[EX_MEM_reg[11:6]]; // writeAddr __ readAddr
+              $display("w %b", EX_MEM_reg[5:0]);
+              $display("dr %b", memoryReg[EX_MEM_reg[5:0]]);
+      
     end
     
     if (EX_MEM_reg[79] == 1) //RegWrite
     begin
-      $display("writeReg");
       memoryReg[EX_MEM_reg[5:0]] = EX_MEM_reg[43:12]; // writeAddr __ ALUresult
     end
   end
